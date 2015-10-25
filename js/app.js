b294a1c17aeb0956c64c0337ba2153559ab3898e
Martin Delille <martin@phonations.com>
2015-10-25 02:35:38 +0100

    implement operation

--- js/app.js
+++ js/app.js
@@ -1,2 +1,57 @@
 $(function() {
+  var lastNumber = 0;
+  var number = 0;
+  var op = '';
+  var updateDisplay = function() {
+    if(number !== '')
+      $('#number').text(number);
+    else
+      $('#number').text(lastNumber);
+  };
+
+  $('.ac').bind('click', function() {
+    lastNumber = 0;
+    number = 0;
+    op = '';
+    updateDisplay();
+  });
+
+  $('.ce').bind('click', function() {
+    number = '';
+    updateDisplay();
+  });
+
+  $('.nb').bind('click', function() {
+    var digit = parseInt($(this).text());
+    number = number * 10 + digit;
+    updateDisplay();
+  });
+
+  $('.op').bind('click', function() {
+    op = $(this).text();
+    if(number !== '') {
+      lastNumber = number;
+      number = '';
+    }
+  });
+
+  $('.equal').bind('click', function() {
+    if(op === '%') {
+      lastNumber = lastNumber % number;
+    }
+    else if(op === '÷') {
+      lastNumber = lastNumber / number;
+    }
+    else if(op === '-') {
+      lastNumber = lastNumber - number;
+    }
+    else {
+      lastNumber = lastNumber + number;
+    }
+    number = '';
+
+    updateDisplay();
+  });
+
+  updateDisplay();
 });
